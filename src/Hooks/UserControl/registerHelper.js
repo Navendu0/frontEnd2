e670764c59ff15685c1a9d0d973ea057d3a792dc
Navendu0 <65621942+Navendu0@users.noreply.github.com>
2022-12-30 21:50:42 +0530

    Update registerHelper.js

--- src/Hooks/UserControl/registerHelper.js
+++ src/Hooks/UserControl/registerHelper.js
@@ -1,10 +1,16 @@
 import axios from "axios"
-import { useState } from "react"
+import { useContext, useState } from "react"
 import { RouterProvider } from "react-router-dom"
 import { domain } from "../../../Util/Address"
 import { url } from "../../Route/Address"
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../App"
+
 
 const registerHelper = () => {
+  const navigate = useNavigate()
+  const { loading, setLoading } = useContext(AppContext)
+
   const { baseUrl, route, login, register, logOut } = url
 
   const [email, setEmail] = useState()
@@ -13,24 +19,35 @@ const registerHelper = () => {
 
 
   const registerAction = async () => {
-    
-    if (password == confirmPassword) {
-     return  alert("Please check password and Confirm password")
+
+    if (password != confirmPassword) {
+      return alert("Please check password and Confirm password")
     }
+    setLoading({ ...loading, msg: "Register User....", isLoading: true })
+
 
     await axios.post(baseUrl + route + register, {
       "username": email,
       "password": password,
       "confirmPassword": confirmPassword
     }).then((res) => {
-      console.log(res.data)
+      //  console.log(res.data)
+      setLoading({ ...loading, msg: res.data.message, isLoading: false })
 
       if (!res.data.success) {
+        setLoading({ ...loading, msg: res.data.message, isLoading: false })
+
         alert(res.data.message)
       }
 
+      navigate("/login")
       // setTimeout(()=>{setHide(false)},3000)
-    }).catch(err => console.log(err.message))
+    }).catch(err => {
+      setLoading({ ...loading, msg: err.message, isLoading: false })
+
+      //   console.log(err.message)
+    }
+    )
 
   }
 
